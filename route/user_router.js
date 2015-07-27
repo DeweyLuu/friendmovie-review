408233ff6804e44512a459d2a0e01ef7cd37f52f
Meng Chen <tamulemon@gmail.com>
2015-07-27 13:10:37 -0700

    correct models and add server.js

--- route/user_router.js
+++ route/user_router.js
@@ -4,29 +4,35 @@ var mongoose = require('mongoose'),
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var User = require('../model/user.js');
-var Movie = require('../model/movie.js');
+var User = require('../models/user.js');
+var Movie = require('../models/movie.js');
 
 module.exports = function(router) {
 	router.use(bodyParser.json());
 	router.route('/users')
 	.get(function(req, res) {})
 	// index page, show all user names
+	// Dewy
 	
 	
 	router.route('/users/')
-	.auth
-	. post
+//	. auth(function(req, res) {})
+	. post(function(req, res) {})
 	// auth, let user signin and create new user
+	// Conlan
 	
 	
 	router.route('/users/:userId')
-	.get
+	.get(function(req, res) {})
 	// pull information about a user, name and all movie
+	// Dewy
 	
-	.post
+	.post(function(req, res) {})
 	// create a new movie, save movie to db and insert id to user
+	// Meng
+	
 	
 	router.route('/users/:userId/:movieId')
-	.delete
-	
+	.delete(function(req, res) {})
+	// Meng
+}
