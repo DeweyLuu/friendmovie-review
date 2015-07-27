efbaf82e681bb9cdbce341c4f4bc4007914d2955
Conlan McNamee <conlanmcnamee@gmail.com>
2015-07-27 13:16:22 -0700

    updated user.js to have bcrypt

--- models/user.js
+++ models/user.js
@@ -11,4 +11,41 @@ var userSchema = mongoose.Schema({
 	}]
 });
 
+userSchema.pre('save', function (next) {
+	var user = this;
+
+	//just a simple check to see if password is being created or modifed
+	if(!user.isModified('password')) return next();
+
+	//creating a random salt, 10 is the default value
+	bcrypt.genSalt(10, function (err, salt) {
+		if(err) {
+			return console.log(err);
+		} else {
+			//hashing the password before the user is saved
+			bcrypt.hash(user.password, salt, function (err, hash) {
+				if (err) {
+					return next(err);
+				} else {
+					//when we get here the users password is swapped with our hash value
+					user.password = hash;
+					next();
+				}
+			});
+		}
+	});
+});
+
+userSchema.methods.comparePassword = function (enteredPass, cb) {
+	//this is the method that will be used when authenticating the passwords
+	bcrypt.compare(enteredPass, this.password, function (err, isMatch) {
+		//isMatch will return true or false
+		if(err) {
+			return cb(err);
+		} else {
+			cb(null, isMatch);
+		}
+	})
+}
+
 module.exports = mongoose.model('User', userSchema);
