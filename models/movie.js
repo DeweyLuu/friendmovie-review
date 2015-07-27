ca8f34894ff54a7c20843af32e4cc556c79ae132
Dewey Luu <deweyluu@gmail.com>
2015-07-27 14:38:59 -0700

    edited movieschema

--- models/movie.js
+++ models/movie.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
 var movieSchema = mongoose.Schema({
-	title: String,
-	year: Number,
+	title: {type: String, require: true},
+	year: {type: Number, require: true},
+	verification: title.split(' ').join().toLowerCase() + year.toString();
 	genre: String
 })
 
