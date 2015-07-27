9b5c2d1428887ff118c766a868cb84d234b7419b
Dewey Luu <deweyluu@gmail.com>
2015-07-27 14:32:05 -0700

    added get route

--- route/user_router.js
+++ route/user_router.js
@@ -10,23 +10,40 @@ var Movie = require('../model/movie.js');
 module.exports = function(router) {
 	router.use(bodyParser.json());
 	router.route('/users')
-	.get(function(req, res) {})
+	.get(function(req, res) {
+		User.find({}, function(err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				res.json(data);
+			}
+		});
+	})
 	// index page, show all user names
-	
-	
+
+
 	router.route('/users/')
 	.auth
 	. post
 	// auth, let user signin and create new user
-	
-	
+
+
 	router.route('/users/:userId')
-	.get
+	.get(function(req, res) {
+		var person = req.params.user;
+		User.findOne({displayName: person}, function(err, data) {
+			if (err) {
+				res.json({msg: 'User not found'});
+			} else {
+				res.json({msg: 'Found user'});
+			}
+		})
+	})
 	// pull information about a user, name and all movie
-	
+
 	.post
 	// create a new movie, save movie to db and insert id to user
-	
+
 	router.route('/users/:userId/:movieId')
 	.delete
-	
+
