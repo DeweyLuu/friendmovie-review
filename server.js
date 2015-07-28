b82720e42c0c06d9d09003ef5fc541f2818dbe3a
Dewey Luu <deweyluu@gmail.com>
2015-07-28 10:25:13 -0700

    removed mongolab pw

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var port = process.env.PORT || 8080;
 
 process.env.MONGO_URL = 'mongodb://localhost/moview_testDB';
 
-mongoose.connect('mongodb://user:password@ds031952.mongolab.com:31952/reelfriends' || process.env.MONGOLAB_URI);
-// || 'mongodb://localhost/friendmovie_review');
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/friendmovie_review');
 
 var apiRouter = express.Router ();
 require('./route/user_router.js')(apiRouter);
