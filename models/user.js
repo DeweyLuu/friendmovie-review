d281da57a961bf9ae8ab9d5b5631ad9d963e8391
Dewey Luu <deweyluu@gmail.com>
2015-07-29 09:23:11 -0700

    added verify middleware

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ var bcrypt = require('bcrypt');
 
 
 var userSchema = new Schema ({
-	logInName: {type: String, unique: true},
+	logInName: {type: String, unique: true, required: true},
 	displayName: String,
-	password: String,
+	password: {type: String, required: true},
 	movies: [{
 //		 _id: false,
 		movie: Schema.Types.ObjectId,
@@ -23,9 +23,9 @@ return bcrypt.hashSync(password, 8, null)
 
 userSchema.methods.comparePassword = function (enteredPass, cb) {
 	//this is the method that will be used when authenticating the passwords
-	return bcrypt.compareSync(enteredPass, this.password); 
+	return bcrypt.compareSync(enteredPass, this.password);
 		//isMatch will return true or false
-		
+
 }
 
 module.exports = mongoose.model('User', userSchema);
