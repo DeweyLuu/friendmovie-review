e3ead60cbc00d26e9c3a2e00fca5cef52598349e
Conlan McNamee <conlanmcnamee@gmail.com>
2015-07-30 11:04:38 -0700

    syncing up with front end objects

--- route/auth_routes.js
+++ route/auth_routes.js
@@ -6,11 +6,11 @@ module.exports = function (router) {
 	router.use(bodyParser.json());
 	router.post('/login', function (req, res) {
 		
-		User.findOne({ logInName: req.body.logInName}, function (err, user) {
+		User.findOne({ logInName: req.body.loginForm.logInName}, function (err, user) {
 //			console.log("are we in here?",user.logInName);
 			if(err) {
 				res.status(500).json({msg: 'server error'});
-			} else if (user.comparePassword(req.body.password) === true) {
+			} else if (user.comparePassword(req.body.loginForm.password) === true) {
 				var token = jwt.sign(user._id, process.env.secret, {expiresInMinutes: 120});
 				res.json({
 					success: true,
